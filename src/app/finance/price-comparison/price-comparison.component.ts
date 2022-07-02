c04a285b7cecc5e696e721912b67f75d40d84566
Stefan Fuchs <stefan@fuchs.pub>
2022-07-02 14:27:42 +0200

    price comparison wip #35

--- src/app/finance/price-comparison/price-comparison.component.ts
+++ src/app/finance/price-comparison/price-comparison.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiReply } from 'src/app/api-reply';
+import { AuthService } from 'src/app/auth.service';
+import { AppConfig, ConfigService } from 'src/app/config.service';
+import { I18nService } from 'src/app/i18n.service';
+import { Settings } from 'src/app/user/settings/settings';
+import { SettingsService } from 'src/app/user/settings/settings.service';
+import { FormatService } from 'src/app/utils/format.service';
+import { ToastsService } from 'src/app/utils/toasts.service';
+import { ReceiptArticle } from '../finance';
 
 @Component({
   selector: 'app-price-comparison',
@@ -7,9 +16,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PriceComparisonComponent implements OnInit {
 
-  constructor() { }
+  articles: { [ key: number]: ReceiptArticle } = {};
+  items: ComparisonItem[][] = [];
+  usersettingsObj: Settings | null = null;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService,
+    private configService: ConfigService,
+    private i18nService: I18nService,
+    private userSettings: SettingsService,
+    public formatService: FormatService,
+    private toastService: ToastsService) {
+    this.userSettings.settings$.subscribe((settings) => {
+      this.usersettingsObj = settings;
+    });
   }
 
+  get config(): AppConfig {
+    return this.configService.config;
+  }
+
+  first(items: ComparisonItem[]) : string {
+    let min = '2099-12-31';
+    items.forEach((i) => { if (i.date < min) min = i.date });
+    return min;
+  }
+
+  i18n(key: string, params: string[] = []): string {
+    return this.i18nService.i18n(key, params);
+  }
+
+  last(items: ComparisonItem[]) : string {
+    let max = '2000-01-01';
+    items.forEach((i) => { if (i.date > max) max = i.date });
+    return max;
+  }
+
+  max(items: ComparisonItem[]) : number {
+    let max = 0.0;
+    items.forEach((i) => { if (i.singleprice > max) max = i.singleprice });
+    return max;
+  }
+
+  min(items: ComparisonItem[]) : number {
+    let min = 999999.0;
+    items.forEach((i) => { if (i.singleprice < min) min = i.singleprice });
+    return min;
+  }
+
+  ngOnInit(): void {
+    let url: string = this.config.api.baseUrl + '/fin/comparison';
+    this.authService.queryApi(url).subscribe((reply: ApiReply) => {
+      if (reply.success) {
+        this.articles = (<ComparisonReply>reply.payload).articles;
+        this.items = Object.values((<ComparisonReply>reply.payload).items);
+        this.sortItems();
+      }
+      console.log(this.articles);
+      console.log(this.items);
+    });
+  }
+
+  sortItems(): void {
+    this.items.sort((a, b) => {
+      return a[0].name > b[0].name ? 1 : a[0].name < b[0].name ? -1 : a[0].search > b[0].search ? 1 : -1 
+    });
+    this.items.forEach((i) => console.log(i[0].name, i[0].search))
+  }
+
+}
+
+export interface ComparisonItem {
+  articleid: number;
+  count: number;
+  date: string;
+  itemid: number;
+  name: string;
+  organic: boolean;
+  receiptid: number;
+  search: string;
+  singleprice: number;
+}
+
+export interface ComparisonReply {
+  articles: { [ key: number]: ReceiptArticle };
+  items: { [ key: number]: ComparisonItem[] };
 }
