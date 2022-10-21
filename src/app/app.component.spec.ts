987b398c44d187a08790816688595611c49cc659
Stefan Fuchs <stefan@fuchs.pub>
2022-10-21 19:55:38 +0200

    first successfull unit test :) :) :)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 import { AuthService } from './auth.service';
 import { ConfigService } from './config.service';
@@ -16,6 +18,10 @@ describe('AppComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule,
+                HttpClientTestingModule
+            ],
             declarations: [
                 AppComponent
             ]
@@ -34,10 +40,4 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain('test app is running!');
-    });
 });
