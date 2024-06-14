bcde4d669b15b3d05272d34b730d490ca927dc24
Stefan Fuchs <stefan@fuchs.pub>
2024-06-14 13:58:50 +0200

    Add new environments

---
+++ src/environments/types.ts
@@ -0,0 +1,30 @@
+import { NavbarItem } from '../app/config.service';
+
+export type Environment = {
+    production: boolean,
+    api: {
+        baseUrl: string,
+        auth: {
+            checkUrl?: string,
+            loginUrl: string,
+            oauth2Providers: { [key: string]: EnvironmentOAuth2Provider }
+        },
+    },
+    l10n: {
+        fallbackLocale: 'de' | 'en' | 'fr',
+    },
+    navigation: {
+        navbarBarItems: NavbarItem[],
+        navbarDropdownItems: NavbarItem[],
+        financeBarItems: NavbarItem[],
+        workBarItems: NavbarItem[]
+    },
+    localStoragePrefix: string
+}
+
+export type EnvironmentOAuth2Provider = {
+    baseUrl: string,
+    clientId: string,
+    clientSecret: string,
+    state: string
+};
