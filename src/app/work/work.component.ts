93849236661dcb18c0d7440d28ed88ca77241a3b
Stefan Fuchs <stefan@fuchs.pub>
2024-07-01 14:56:43 +0200

    Remove unused code

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment.dev';
 import { NavbarItem } from '../config.service';
-import { I18nService } from '../i18n.service';
-import { L10nArchiveLocale } from '../l10n/l10n.types';
 
 @Component({
   selector: 'app-work',
@@ -12,19 +10,5 @@ import { L10nArchiveLocale } from '../l10n/l10n.types';
 export class WorkComponent {
 
   navitems: NavbarItem[] = environment.navigation.workBarItems;
-
-  constructor(
-    private i18nService: I18nService
-  ) {
-    this.i18nService.setTitle(this.i18nstr.work.pagetitle);
-  }
-
-  /**
-   * Getter for i18n localization strings.
-   * @returns The localization strings.
-   */
-  get i18nstr(): L10nArchiveLocale {
-    return this.i18nService.str;
-  }
 
 }
